ff625324873e35eb7fe443fa94eb64314ee5dfd0
Ming Gong <30853020+hjkl3692008@users.noreply.github.com>
2019-10-31 16:31:06 -0400

    update how to get path

--- catalog/image_deal.py
+++ catalog/image_deal.py
@@ -9,8 +9,8 @@ def encode(o_image, wm):
     name = o_image
 
     path = get_path()
-    o_image = it.load_image(path+"\\"+name)
-    wm = it.load_image(path+"\\"+wm)
+    o_image = it.load_image(join_path(path, name))
+    wm = it.load_image(join_path(path, wm))
 
     wm = it.complement(wm)
 
@@ -22,7 +22,8 @@ def encode(o_image, wm):
 
     final_img = it.real(it.ifft(sum_image))
 
-    it.save_image(final_img, path+"\\bwm_"+name)
+    it.save_image(final_img, join_path(path, "bwm_"+name))
+    it.save_image(final_img, join_path(path, name))
 
     return "bwm_"+name, "media/bwm_"+name
 
@@ -32,17 +33,21 @@ def decode(o_image, bwm_image):
     name = o_image
 
     path = get_path()
-    o_image = it.load_image(path + "\\" + name)
-    bwm_image = it.load_image(path + "\\" + bwm_image)
+    o_image = it.load_image(join_path(path, name))
+    bwm_image = it.load_image(join_path(path, bwm_image))
 
     wm = (it.fft(bwm_image) - it.fft(o_image)) / alpha
 
     wm = it.reverse_shuffle(it.real(wm))
 
-    it.save_image(wm, path+"\\wm_"+name)
+    it.save_image(wm, join_path(path, "wm_"+name))
 
     return "wm_"+name, "media/wm_"+name
 
 
 def get_path():
     return os.path.join(os.getcwd(), 'catalog', 'media')
+
+
+def join_path(path1, path2):
+    return os.path.join(path1, path2)
