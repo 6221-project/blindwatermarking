ebad5ff9a658eaf14cf2e92ba0ae9f40633a4c7b
sjq <QMAC@AxisdeMacBook-Pro.local>
2019-11-01 15:50:01 -0400

    modified:   catalog/image_tool.py

--- catalog/image_tool.py
+++ catalog/image_tool.py
@@ -6,6 +6,9 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+#align image config
+MAX_FEATURES = 500
+GOOD_MATCH_PERCENT = 0.15
 
 # load image
 def load_image(path):
@@ -115,6 +118,7 @@ def reverse_shuffle(img, seed=8888):
             img2[m[i]][n[j]] = img[i][j]
     return img2
 
+#optimal shape
 def optimal_shape(img):
     rows = img.shape[0]
     cols = img.shape[1]
@@ -129,3 +133,46 @@ def optimal_shape(img):
 def resize(img, size):
     s_img = cv2.resize(img, size, interpolation=cv2.INTER_CUBIC)
     return s_img
+
+#align image
+def alignImages(im1, im2):
+    # Convert images to grayscale
+    im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+     
+    # Detect ORB features and compute descriptors.
+    orb = cv2.ORB_create(MAX_FEATURES)
+    keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+     
+    # Match features.
+    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = matcher.match(descriptors1, descriptors2, None)
+     
+    # Sort matches by score
+    matches.sort(key=lambda x: x.distance, reverse=False)
+ 
+    # Remove not so good matches
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
+ 
+    # Draw top matches
+    imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
+    cv2.imwrite("matches.jpg", imMatches)
+     
+    # Extract location of good matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+ 
+    for i, match in enumerate(matches):
+        points1[i, :] = keypoints1[match.queryIdx].pt
+        points2[i, :] = keypoints2[match.trainIdx].pt
+     
+    # Find homography
+    h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+ 
+    # Use homography
+    height, width, channels = im2.shape
+    im1Reg = cv2.warpPerspective(im1, h, (width, height))
+     
+    return im1Reg, h
