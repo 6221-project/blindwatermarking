d8beec7df82e3fb4f76fa77e98bf340c677e0c53
sjq <QMAC@AxisdeMacBook-Pro.local>
2019-11-01 15:56:23 -0400

    delete cached

--- catalog/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class image(models.Model):
-    image_name = models.CharField(verbose_name='image_name', max_length=32)
-    image_type = models.CharField(verbose_name='image_type', max_length=32)
-    image = models.FileField(verbose_name='image')
