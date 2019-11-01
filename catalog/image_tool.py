66e921162b5be9cde669e18c51cf6f8233b4c51f
sjq <QMAC@AxisdeMacBook-Pro.local>
2019-11-01 14:47:09 -0400

    modified:   catalog/image_tool.py

--- catalog/image_tool.py
+++ catalog/image_tool.py
@@ -112,3 +112,12 @@ def reverse_shuffle(img, seed=8888):
             # img2[m[i]][n[j]] = np.uint8(img[i][j])
             img2[m[i]][n[j]] = img[i][j]
     return img2
+
+def optimal_shape(img):
+    rows = img.shape[0]
+    cols = img.shape[1]
+    nrows = cv2.getOptimalDFTSize(rows)
+    ncols = cv2.getOptimalDFTSize(cols)
+    nimg = np.zeros((nrows,ncols,img.shape[2]))
+    nimg[:rows,:cols] = img
+    return nimg
