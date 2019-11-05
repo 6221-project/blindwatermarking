58768cbb413b20acf56583f165fd6a7042bc6d97
Jiaqiu Shen <405739964@qq.com>
2019-11-05 16:02:33 -0500

    add optimal_shape delete error save

--- catalog/image_deal.py
+++ catalog/image_deal.py
@@ -9,18 +9,15 @@ def encode(o_image, wm):
     name = o_image
 
     path = get_path()
-    o_image = it.load_image(join_path(path, name))
-    wm = it.load_image(join_path(path, wm))
-    print(wm.shape)
-
+    o_image = it.optimal_shape(it.load_image(join_path(path, name)))
+    wm = it.optimal_shape(it.load_image(join_path(path, wm)))
     wm_bgr, wm = it.complement(wm)
+
     # Reshape size of watermark and flip it
     wm_shape = ((int)(o_image.shape[1] / 2), (int)(o_image.shape[0] / 2))
     r_wm = it.resize(wm, wm_shape)
-    print(r_wm.shape)
     s_wm = it.fill_image(r_wm, o_image.shape)
     f_wm = it.flip(s_wm) + s_wm
-    print(f_wm.shape)
     #s_wm = it.shuffle_image_with_shape(wm, wm_shape)
 
     f_image = it.shift(it.fft(o_image))
@@ -31,7 +28,6 @@ def encode(o_image, wm):
     final_img = it.real(it.ifft(it.ishift(sum_image)))
 
     it.save_image(final_img, join_path(path, "bwm_"+name))
-    it.save_image(final_img, join_path(path, name))
 
     return "bwm_"+name, "media/bwm_"+name
 
@@ -46,11 +42,10 @@ def decode(o_image, bwm_image):
     o_image = it.load_image(join_path(path, o_image))
 
     # align
-    bwm_image, h = it.alignImages(bwm_image, o_image)
+    #bwm_image, h = it.alignImages(bwm_image, o_image)
 
     wm = it.real(it.shift(it.fft(bwm_image)))
     # wm = (it.fft(bwm_image) - it.fft(o_image)) / alpha
-
     # wm = it.reverse_shuffle(it.real(wm))
 
     it.save_image(wm, join_path(path, "wm_"+name))
